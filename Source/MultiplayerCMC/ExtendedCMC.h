584c32672033f500461430f76092fd62f0ae19a2
robinpipirs <robinpipirs@gmail.com>
2023-01-02 12:17:23 +0100

    added source code

---
+++ Source/MultiplayerCMC/ExtendedCMC.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "ExtendedCMC.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERCMC_API UExtendedCMC : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+};
